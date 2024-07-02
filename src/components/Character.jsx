64a362fe6899626783e854f6e5b79bd9e75b951a
freyder_montano <fremontano24@gmail.con>
2024-07-01 21:43:18 -0400

    consumiendo api con axio en mi context y pasar la informacion al componente character

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -2,14 +2,29 @@ import React, { useContext } from 'react';
 import CharactersContext from '../context/charactersContext';
 
 export const Character = () => {
-    const valor = useContext(CharactersContext);
+    const { characters } = useContext(CharactersContext);
+    // console.log('%c personjes', 'background: blue; font-size: 15px; color: white;')
+    // console.log(characters)
 
     return (
         <div className="row">
-            <div className="col-4">
-                <p>Imagen</p>
-                <h2>{valor}</h2>
-            </div>
+
+            {
+                characters.map((character) => (
+                    <div className="col-md-3" key={character.id}>
+                        <div className="card mb-4">
+                            <img src={character.image} className="card-img-top" alt={character.name} />
+                            <div className="card-body">
+                                <h5 className="card-title">{character.name}</h5>
+                                <p className="card-text">Status: {character.status}</p>
+                                <p className="card-text">Species: {character.species}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+
+
         </div>
     );
 };
--- src/context/charactersContext.jsx
+++ src/context/charactersContext.jsx
@@ -1,12 +1,37 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import Axios from 'axios';
 
 // Crear el Contexto
 const CharactersContext = createContext();
 
+
 // Crear el Proveedor del Contexto
 export const CharactersContextProvider = ({ children }) => {
+    // State y Funciones
+    const [characters, setCharacters] = useState([]);
+
+
+
+
+    // useEffect
+    useEffect(() => {
+        Axios.get('https://rickandmortyapi.com/api/character')
+            .then(response => {
+                if (response.status === 200) {
+                    setCharacters(response.data.results)
+                }
+            })
+
+    }, []);
+
+
+
+
     return (
-        <CharactersContext.Provider value={'Rick and Morty'}>
+        <CharactersContext.Provider value={{
+            characters
+        }}>
+
             {children}
         </CharactersContext.Provider>
     );
